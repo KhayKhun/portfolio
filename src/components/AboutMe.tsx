a82d591c35d619c36539c687d882e305bcca73ae
KhayKhun <saikhaykhunmong@gmail.com>
2024-03-20 01:19:42 +0630

    enable loop, some css fixes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,37 +1,29 @@
-import React, { useState } from "react";
+import { useRef, useEffect } from "react";
 import Typed from "typed.js";
 
 export default function AboutMe() {
-  // Create reference to store the DOM element containing the animation
-  const el = React.useRef(null);
-  const [complete,setComplete] = useState(false);
+  const el = useRef(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const typed = new Typed(el.current, {
       strings: [
-        "Hello World!",
+        "Hi!",
         "I'm Sai Khay Khun Mong.",
         "Creating bugs since 2022.",
-        "<a href='https://www.facebook.com/simpleyourweb'>Like my Facebook page....</a>",
+        "<a href='https://www.facebook.com/simpleyourweb'>Like my Facebook page.</a>",
       ],
-      typeSpeed: 20,
-      loop: false,
-      onComplete: () => {
-        setComplete(true);
-      },
+      typeSpeed: 30,
+      loop: true,
     });
 
     return () => {
-      // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
     };
   }, []);
 
   return (
     <div
-      className={`${
-        complete && "underline tracking-wide plause"
-      }  mono-font z-10 fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-green-400 font-bold font-mono`}
+      className={`mono-font z-10 fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-green-400 font-bold font-mono`}
     >
       <span ref={el} />
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ import {
 
 const NavBar = () => {
   return (
-    <div className="text-xl sm:text-2xl xl:text-3xl 2xl:text-4xl text-cyan-400">
+    <div className="text-2xl xl:text-3xl 2xl:text-4xl text-cyan-400">
       <div className=" z-10 fixed gap-3 flex sm:left-auto sm:bottom-auto sm:right-0 sm:flex-col sm:top-[50%] sm:-translate-y-[50%] sm:translate-x-0 sm:rounded-r-none sm:border-r-0 sm:border-b-[0.1px] bg-white bg-opacity-10 p-3 sm:rounded-l-md border-[0.1px] border-cyan-300 left-[50%] bottom-0 -translate-x-[50%] border-b-0 rounded-t-md">
         <a
           className="relative"
@@ -49,7 +49,7 @@ const NavBar = () => {
           href="https://www.facebook.com/simpleyourweb"
           target="blank"
         >
-          <div className="absolute w-full h-full top-0 left-0 bg-cyan-400 hover:animate-ping rounded-full opacity-0 hover:opacity-100" />
+          <div className="absolute w-full h-full top-0 left-0 bg-cyan-400 animate-ping rounded-full opacity-100" />
           <FacebookIcon />
         </a>
         <a
